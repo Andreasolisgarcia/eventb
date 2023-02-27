1bb9fb385e959ffe64f5ea12b014162bf3eaf3ee
Rea <andrea.solisg@gmail.com>
2023-02-27 08:44:12 +0100

    .env crée et registaions fait

--- Gemfile
+++ Gemfile
@@ -73,3 +73,4 @@ end
 
 gem "faker"
 gem "devise"
+gem 'dotenv-rails'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,27 @@
 class UsersController < ApplicationController
-  def index
-    
-  end
 
   def show
+    @user = User.find(current_user.id)
   end
 
   def new
+    @user = User.new
   end
 
   def edit
+    @user = User.find(current_user.id)
   end
-
-  def create
-  end
-
+  
   def update
+      @user= User.find(current_user.id)
+      if @user.update(user_params)
+        flash[:success] = "You got it! Your profile is updated!"
+        redirect_to user_path(@user)
+      end
   end
-
-  def destroy
+  
+  private 
+  def user_params
+      params.require(:user).permit(:first_name, :last_name, :description)
   end
 end
